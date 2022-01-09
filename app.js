c0b23daea32bfe72816989d12ae2de8818397f94
Anish Jakka Singaraiah <anishjakka@gmail.com>
2022-01-09 21:17:36 +0530

    update es6+

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var intialPrice = document.querySelector("#intial-price");
-var stockQuantity = document.querySelector("#stocks-quantity");
-var currentPrice = document.querySelector("#current-price");
-var submitBtn = document.querySelector("#submit-btn");
-var output = document.querySelector("#output-box");
+const intialPrice = document.querySelector("#intial-price");
+const stockQuantity = document.querySelector("#stocks-quantity");
+const currentPrice = document.querySelector("#current-price");
+const submitBtn = document.querySelector("#submit-btn");
+const output = document.querySelector("#output-box");
 
 const submitHandler = () => {
   const ip = intialPrice.value;
@@ -20,17 +20,17 @@ const submitHandler = () => {
 };
 
 const calculateProfitAndLoss = (initial, quantity, current) => {
-  initial = Number(initial);
-  quantity = Number(quantity);
-  current = Number(current);
+  const initial = Number(initial);
+  const quantity = Number(quantity);
+  const current = Number(current);
   if (initial > current) {
-    var loss = (initial - current) * quantity;
-    var lossPercentage = (loss / initial) * 100;
+    const loss = (initial - current) * quantity;
+    const lossPercentage = (loss / initial) * 100;
 
     showOutput(`Hey the loss is ${loss} and the percent is ${lossPercentage}%`);
   } else if (initial < current) {
-    var profit = (current - initial) * quantity;
-    var profitPercentage = (profit / initial) * 100;
+    const profit = (current - initial) * quantity;
+    const profitPercentage = (profit / initial) * 100;
     showOutput(
       `Hey the profit is ${profit} and the percent is ${profitPercentage}%`
     );
